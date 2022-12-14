05a7c660ccf4b9fcd8252db6caab6d04f55c096a
DESKTOP-BRB141Q\PC <longeithershort@gmail.com>
2022-12-14 09:40:08 +0300

    fix expanded inside flex

--- example/lib/screens/message_screen.dart
+++ example/lib/screens/message_screen.dart
@@ -29,7 +29,7 @@ class _MessageScreenState extends StatelessWidget{
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20.0),
           ),
-          child: Stack
+          child: Column
             (
             children:
             <Widget>
